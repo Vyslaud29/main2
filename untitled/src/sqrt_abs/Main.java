095a984926287726137a0868240ed9be6f361f77
Vyslaud29 <pawelnoras2@gmail.com>
2023-10-05 09:58:33 +0200

    hurgorei

--- untitled/src/sqrt_abs/Main.java
+++ untitled/src/sqrt_abs/Main.java
@@ -1,122 +1,27 @@
-package sqrt_abs;
-import java.util.Scanner;
-
-import static java.lang.Math.max;
-import static java.lang.Math.pow;
-import static java.lang.System.in;
-
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
-
-    public static void main(String[] args) {
-
-            Double a, b, wynik = null;
-            int wybor;
-
-            System.out.println("jakie dzialanie chcesz wykonac :" );
-            System.out.println("1. dodawanie ");
-            System.out.println("2. odejmowanie");
-            System.out.println("3. mnozenie");
-            System.out.println("4. dzielenie");
-            System.out.println("5. Potęgowanie");
-            System.out.println("6. Pierwiastkowanie");
-            System.out.println("7. Modulo");
-
-            Scanner scanner = new Scanner(in);
-            wybor = scanner.nextInt();
-
-            switch(wybor){
-                case 1:
-                    System.out.println("Podaj 1 liczbe: ");
-                    a = scanner.nextDouble();
-                    System.out.println("Podaj 2 liczbe: ");
-                    b = scanner.nextDouble();
-                    wynik = a + b;
-                    System.out.println("Wynik wynosi: " +wynik);
-                    scanner.close();
-                    break;
-
-                case 2:
-                    System.out.println("Podaj 1 liczbe: ");
-                    a = scanner.nextDouble();
-                    System.out.println("Podaj 2 liczbe: ");
-                    b = scanner.nextDouble();
-                    wynik = a - b;
-                    System.out.println("Wynik wynosi: " +wynik);
-                    scanner.close();
-                    break;
-
-                case 3:
-                    System.out.println("Podaj 1 liczbe: ");
-                    a = scanner.nextDouble();
-                    System.out.println("Podaj 2 liczbe: ");
-                    b = scanner.nextDouble();
-                    wynik = a*b;
-                    System.out.println("Wynik wynosi: " +wynik);
-                    scanner.close();
-                    break;
-
-                case 4:
-                    System.out.println("Podaj 1 liczbe: ");
-                    a = scanner.nextDouble();
-                    System.out.println("Podaj 2 liczbe: ");
-                    b = scanner.nextDouble();
-                    if( b == 0){
-                        System.out.println("nie dzieli sie przez zero!!!");
-                    }
-                    else{ wynik = a/b;
-                    }
-                    System.out.println("Wynik wynosi: " +wynik);
-                    scanner.close();
-                    break;
-
-
-                case 5:
-                    System.out.println("Podaj 1 liczbe: ");
-                    a = scanner.nextDouble();
-                    System.out.println("Podaj potęgę do ktorej 1 liczba zostanie podniesiona: ");
-                    b = scanner.nextDouble();
-                    wynik =pow(a,b);
-                    System.out.println("Wynik wynosi: " +wynik);
-                    scanner.close();
-                    break;
-
-                case 6:
-                    System.out.println("Podaj 1 liczbe: ");
-                    a = scanner.nextDouble();
-                    wynik = Math.sqrt(a);
-                    System.out.println("Wynik wynosi: " +wynik);
-                    scanner.close();
-                    break;
-
-                case 7:
-                    System.out.println("Podaj 1 liczbe: ");
-                    a = scanner.nextDouble();
-                    System.out.println("Podaj 2 liczbe: ");
-                    b = scanner.nextDouble();
-                    wynik = a%b;
-                    System.out.println("Wynik wynosi: " +wynik);
-                    scanner.close();
-                    break;
-            }
-        }
-
-
-
-
-}
-
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
+package sqrt_abs;//package sqrt_abs;
+//import java.util.Scanner;
+//
+//import static java.lang.Math.max;
+//import static java.lang.Math.pow;
+//import static java.lang.System.in;
+//
+//Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+//then press Enter. You can now see whitespace characters in your code.
+//public class Main {
+//
+//    public static void main(String[] args) {
+//
+//
+//        // Press Alt+Enter with your caret at the highlighted text to see how
+//        // IntelliJ IDEA suggests fixing it.
 //        System.out.printf("champagnepapi");
 //
 //        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-//        for (int i = 1; i <= 1; i++) {
+//       for (int i = 1; i <= 1; i++) {
 //
-//            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-//            // for you, but you can always add more by pressing Ctrl+F8.
-//            System.out.println("i = " + i);
+//           // Press Shift+F9 to start debugging your code. We have set one breakpoint
+//           // for you, but you can always add more by pressing Ctrl+F8.
+//          System.out.println("i = " + i);
 //
 //        }
 //
@@ -132,15 +37,15 @@ public class Main {
 //
 //        char w = 'x';
 //
-  //      String name = "champagnepapi";
- //       System.out.println(name.toUpperCase());
+//      String name = "champagnepapi";
+//       System.out.println(name.toUpperCase());
 //        System.out.println(name.toLowerCase());
-
+//
 //        int a;
 //        for (a=1;a<16;++a)
 //        System.out.println(a);
 //        char();
-
+//
 //
 //        int i = 1;
 //        for (i = 1; i <= 5; ++i); {
@@ -160,7 +65,7 @@ public class Main {
 //    }
 //        System.out.println(nazwa);
 //b();
-////c();
+//c();
 //imie();
 //String imie = "essa";
 //}
@@ -172,4 +77,60 @@ public class Main {
 //        System.out.println(imie);
 
 
+import java.util.Scanner;
 
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Jakie działanie chcesz wykonać:");
+        System.out.println("1. Dodawanie");
+        System.out.println("2. Odejmowanie");
+        System.out.println("3. Mnożenie");
+        System.out.println("4. Dzielenie");
+        System.out.println("5. Potęgowanie");
+        System.out.println("6. Pierwiastkowanie");
+        System.out.println("7. Modulo");
+
+        int wybor = scanner.nextInt();
+        double a, b, wynik = 0.0;
+
+        System.out.println("Podaj pierwszą liczbę: ");
+        a = scanner.nextDouble();
+        System.out.println("Podaj drugą liczbę: ");
+        b = scanner.nextDouble();
+
+        switch (wybor) {
+            case 1:
+                wynik = a + b;
+                break;
+            case 2:
+                wynik = a - b;
+                break;
+            case 3:
+                wynik = a * b;
+                break;
+            case 4:
+                if (b == 0) {
+                    System.out.println("nie mozna dielic przez zero");
+                    scanner.close();
+                    return;
+                }
+                wynik = a / b;
+                break;
+            case 5:
+                wynik = Math.pow(a, b);
+                break;
+            case 6:
+                wynik = Math.sqrt(a);
+                break;
+            case 7:
+                wynik = a % b;
+                break;
+        }
+
+        System.out.println("wynik wynosi: " + wynik);
+        scanner.close();
+    }
+}
